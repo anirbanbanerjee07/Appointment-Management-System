4a3acbaf0d270a5115ed49c53e9b94411cf5c9e7
Anirban Banerjee <144148335+anirbanbanerjee07@users.noreply.github.com>
2026-07-12 22:16:59 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,17 +1,11 @@
 <?php
-$servername = $_ENV['DB_HOST'];
-$username   = $_ENV['DB_USER'];
-$password   = $_ENV['DB_PASSWORD'];
-$dbname     = $_ENV['DB_NAME'];
-$port       = $_ENV['DB_PORT'];
+$servername = "localhost";
+$username = "root";
+$password = ""; 
+$dbname = "edoc";
+$port = 4306;
 
-$database = new mysqli(
-    $servername,
-    $username,
-    $password,
-    $dbname,
-    $port
-);
+$database = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($database->connect_error) {
     die("Connection failed: " . $database->connect_error);
